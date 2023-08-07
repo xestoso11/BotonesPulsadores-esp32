20ea370092212aee12d56f07542c05a9ef0db571
xestoso11 <84078701+xestoso11@users.noreply.github.com>
2023-08-07 17:03:28 +0200

    Update pulsador_2patas.ino

--- pulsador_2patas/pulsador_2patas.ino
+++ pulsador_2patas/pulsador_2patas.ino
@@ -1,21 +1,26 @@
-// Indicamos el pin de la placa en el que conectamos el boton
+// Indicamos el pin de la placa en el que conectamos el botón
 const int botonPin12 = 12;
-// definimos la variable que va a recibir los valores del boton para sabes si esta activo o apagado
+// definimos la variable que va a recibir los valores del botón para saber si esta activo o apagado
 int boton1 = 0;
 
 
 void setup() {
+  // Inicializamos el puerto serial, para poder leer a través de el, entre los parétesis le indicaremos la velocidad
   Serial.begin(115200);
+  // Nos mostrara al leer el puerto serial "Iniciado" cuando arranque nuestra placa
   Serial.println("Iniciado");
-  // pinMode indicamos el pin que se va a leer y gracias a INPUT_PULLUP nos permite no tener que usar resistencias para que funcionen los botones
+  // pinMode indicamos el pin que se va a leer y gracias a INPUT_PULLUP nos permite no tener que usar resistencias para que funcionen los botones, ya que usara la propias resistencias de la placa
   pinMode(botonPin12, INPUT_PULLUP);
 }
 
 void loop() {
   //leemos el pin en el que está el boton y le asignamos el valor a la variable que creamos al principio
   boton1 = digitalRead(botonPin12);
+  // Éste if nos permite saber cuando se activa el botón, se iguala a 0 cuando se active ya que usamos INPUT_PULLUP al inicial, si usáramos INPUT tendriamos que igualarlo a 1
   if (boton1 == 0) {
+    // Nos indica que el botón se activo por el puerto serial
     Serial.println("boton1 activo");
+    // Nos indica el valor del botón que recibimos por el puerto serial, será siempre 0 ya que está dentro del if
     Serial.println(boton1);
   }
   delay(1000);
